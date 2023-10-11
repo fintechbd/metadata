b2e75f4e9820ff13df90b81d34f50e4411131466
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-12 01:05:11 +0600

    LP-18 fix: automated testing configuration successful

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,6 +12,7 @@ class TestCase extends Orchestra
         parent::setUp();
     }
 
+
     protected function getPackageProviders($app)
     {
         return [
@@ -21,11 +22,27 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'mysql');
+        config()->set('app.env', 'testing');
+        config()->set('database.default', 'testing');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_metadata_table.php.stub';
-        $migration->up();
-        */
+
+        $migrations = [
+            include __DIR__ . '/../database/migrations/2023_09_22_190520_create_countries_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_22_190534_create_states_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_22_190542_create_cities_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_22_190551_create_banks_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_22_190658_create_bank_branches_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201014_create_regions_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201022_create_subregions_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201134_create_fund_sources_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201147_create_occupations_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201201_create_relations_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201230_create_remittance_purposes_table.php',
+            include __DIR__ . '/../database/migrations/2023_10_07_213147_create_id_doc_types_table.php'
+        ];
+
+        foreach ($migrations as $migration) {
+            $migration->up();
+        }
     }
 }
