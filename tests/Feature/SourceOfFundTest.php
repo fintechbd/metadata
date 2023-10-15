4f26b9eb1ccf1bca8c50f3f7aa720aecca2c13b6
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-16 03:10:54 +0600

    LP-41 default crud test case write

--- tests/Feature/SourceOfFundTest.php
+++ tests/Feature/SourceOfFundTest.php
@@ -7,99 +7,73 @@ use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 use function Pest\Laravel\putJson;
 
-function createCountry()
+function createFundSources()
 {
-    return \Fintech\MetaData\Facades\MetaData::country()->create([
-        "country_id" => Str::random(20),
-        "name" => "BD",
-        "code" => "BGD",
-        "enabled" => "880",
-        "fund_source_data" => "Dhaka",
+    return \Fintech\MetaData\Facades\MetaData::fundSource()->create([
+        "name" => Str::random(20),
+        "code" => "SOF-SOF",
+        "country_id" => 1,
+        "fund_source_data" => [],
     ]);
 }
 
-test('country list', function () {
-    getJson('/api/metadata/countries')->assertStatus(200);
+test('fund source list', function () {
+    getJson('/api/metadata/fund-sources')->assertStatus(200);
 });
 
-test('country create validation', function () {
-    postJson('/api/metadata/countries', [
-        "name" => '',
-        "iso2" => "BD",
-        "iso3" => "BGD",
-        "phone_code" => "880",
-        "capital" => "Dhaka",
-        "currency" => "BDT",
-        "currency_name" => "Bangladeshi Taka",
-        "currency_symbol" => "$",
-        "nationality" => "Bangladeshi",
-        "region_id" => "1",
-        "country_id" => "1",
-        "language" => "bn",
-        "enabled" => "1",
-        "emoji" => "",
-        "latitude" => "90.000",
-        "longitude" => "23.231",
-        "timezones" => [],
-        "country_data" => []
-    ])->assertStatus(422);
-});
-
-test('country created', function () {
-    postJson('/api/metadata/countries', [
-        "name" => Str::random(20),
-        "iso2" => "BD",
-        "iso3" => "BGD",
-        "phone_code" => "880",
-        "capital" => "Dhaka",
-        "currency" => "BDT",
-        "currency_name" => "Bangladeshi Taka",
-        "currency_symbol" => "$",
-        "nationality" => "Bangladeshi",
-        "region_id" => "1",
-        "country_id" => "1",
-        "language" => "bn",
-        "enabled" => "1",
-        "emoji" => "",
-        "latitude" => "90.000",
-        "longitude" => "23.231",
-        "timezones" => [],
-        "country_data" => []
+test('fund source created', function () {
+    postJson('/api/metadata/fund-sources', [
+        "name" => 'Business',
+        "code" => "Business",
+        "country_id" => 1,
+        "fund_source_data" => [],
     ])->assertStatus(201);
 });
 
-test('country not found', function () {
-    createCountry();
-    getJson('/api/metadata/countries/100')->assertStatus(404);
+test('fund source not found', function () {
+    createFundSources();
+    getJson('/api/metadata/fund-sources/100')->assertStatus(404);
 });
 
-test('country detail', function () {
-    createCountry();
-    getJson('/api/metadata/countries/1')->assertStatus(200);
+test('fund source detail', function () {
+    createFundSources();
+    getJson('/api/metadata/fund-sources/1')->assertStatus(200);
 });
 
-test('country update validation', function () {
-    createCountry();
-    putJson('/api/metadata/countries/1', [
-        'name' => '',
-        'country_data' => ['vendors' => 'emq'],
+test('fund source update validation', function () {
+    createFundSources();
+    putJson('/api/metadata/fund-sources/1', [
+        "name" => '',
+        "code" => "Business",
+        "country_id" => 1,
+        "fund_source_data" => [],
     ])->assertStatus(422);
 });
 
-test('country updated', function () {
-    createCountry();
-    putJson('/api/metadata/countries/1', [
+test('fund source updated', function () {
+    createFundSources();
+    putJson('/api/metadata/fund-sources/1', [
         'name' => Str::random(20),
     ])->assertStatus(200);
 });
 
-test('country deleted', function () {
-    createCountry();
-    deleteJson('/api/metadata/countries/1')->assertStatus(200);
+test('fund source deleted', function () {
+    createFundSources();
+    deleteJson('/api/metadata/fund-sources/1')->assertStatus(200);
 });
 
-test('country restored', function () {
-    $country = createCountry();
-    $country->delete();
-    postJson('/api/metadata/countries/1/restore')->assertStatus(200);
+test('fund source restored', function () {
+    $fundSource = createFundSources();
+    $fundSource->delete();
+    postJson('/api/metadata/fund-sources/1/restore')->assertStatus(200);
+});
+
+test('fund source code field validation', function () {
+    postJson('/api/metadata/fund-sources', [
+        "name" => 'Business',
+        "code" => "Business",
+        "country_id" => 1,
+        "fund_source_data" => [],
+    ])->dd();
+    //assertStatus(201);
 });
