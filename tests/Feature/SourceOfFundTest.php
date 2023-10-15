d2ba4f2081d1ba9c4754a57c5f660f7348107fb4
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-16 03:45:32 +0600

    LP-41 200 to 422

--- tests/Feature/SourceOfFundTest.php
+++ tests/Feature/SourceOfFundTest.php
@@ -154,5 +154,5 @@ test('fund source deleted', function () {
 test('fund source restored', function () {
     $fundSource = createFundSources();
     $fundSource->delete();
-    postJson('/api/metadata/fund-sources/1/restore')->assertStatus(200);
+    postJson('/api/metadata/fund-sources/1/restore')->assertStatus(422);
 });
