b1b8a60d16b20f56ab1c1a49855ae7e90c3e274b
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-16 03:55:10 +0600

    LP-41 rollback

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,12 +3,10 @@
 namespace Fintech\MetaData\Tests;
 
 use Fintech\MetaData\MetaDataServiceProvider;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    use DatabaseMigrations;
     protected function setUp(): void
     {
         parent::setUp();
@@ -29,18 +27,18 @@ class TestCase extends Orchestra
 
 
         $migrations = [
-//            include __DIR__ . '/../database/migrations/2023_09_23_201014_create_regions_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_23_201022_create_subregions_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_22_190520_create_countries_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_22_190534_create_states_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_22_190542_create_cities_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_22_190551_create_banks_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_22_190658_create_bank_branches_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_23_201134_create_fund_sources_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_23_201147_create_occupations_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_23_201201_create_relations_table.php',
-//            include __DIR__ . '/../database/migrations/2023_09_23_201230_create_remittance_purposes_table.php',
-//            include __DIR__ . '/../database/migrations/2023_10_07_213147_create_id_doc_types_table.php'
+            include __DIR__ . '/../database/migrations/2023_09_23_201014_create_regions_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201022_create_subregions_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_22_190520_create_countries_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_22_190534_create_states_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_22_190542_create_cities_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_22_190551_create_banks_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_22_190658_create_bank_branches_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201134_create_fund_sources_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201147_create_occupations_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201201_create_relations_table.php',
+            include __DIR__ . '/../database/migrations/2023_09_23_201230_create_remittance_purposes_table.php',
+            include __DIR__ . '/../database/migrations/2023_10_07_213147_create_id_doc_types_table.php'
         ];
 
         foreach ($migrations as $migration) {
