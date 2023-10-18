010a37c6aba754aa9415f1ccf3a26a7b2bcc4598
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-18 09:10:35 +0600

    LP-44 Generating Test Case Class

---
+++ tests/Feature/RemittancePurposeTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Str;
+
+use function Pest\Laravel\deleteJson;
+use function Pest\Laravel\getJson;
+use function Pest\Laravel\postJson;
+use function Pest\Laravel\putJson;
+
