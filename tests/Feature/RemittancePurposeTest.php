3b9f40ca8c31f308f273c8262342e1be1b41d820
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-18 09:11:01 +0600

    LP-44 Call Model Class for Create Test Case

--- tests/Feature/RemittancePurposeTest.php
+++ tests/Feature/RemittancePurposeTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 use function Pest\Laravel\deleteJson;
@@ -7,3 +8,12 @@ use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 use function Pest\Laravel\putJson;
 
+function createRemittancePurposes(): Model|\MongoDB\Laravel\Eloquent\Model|null
+{
+    return \Fintech\MetaData\Facades\MetaData::remittancePurpose()->create([
+        "name" => Str::random(20),
+        "code" => "SOF-SOF",
+        "country_id" => 1,
+        "remittance_purpose_data" => [],
+    ]);
+}
