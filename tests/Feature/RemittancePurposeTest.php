a7ad6173f8ceed55e3025a86f2d516e45525f20d
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-18 09:22:05 +0600

    LP-44 Remittance Purpose test done

--- tests/Feature/RemittancePurposeTest.php
+++ tests/Feature/RemittancePurposeTest.php
@@ -22,54 +22,54 @@ test('Remittance Purpose list', function () {
 });
 
 test('Remittance Purpose create for blank all field validation expect status code 422', function () {
-    $fundSource = postJson('/api/metadata/remittance-purposes', [
+    $remittancePurpose = postJson('/api/metadata/remittance-purposes', [
         "name" => '',
         "code" => "",
         "country_id" => '',
         "remittance_purpose_data" => [],
     ])->assertStatus(422);
-    //expect($fundSource['message'])->toBe('The name field is required. (and 1 more error)');
+    //expect($remittancePurpose['message'])->toBe('The name field is required. (and 1 more error)');
 });
 
 test('Remittance Purpose create for blank name field validation expect The name field is required.', function () {
-    $fundSource = postJson('/api/metadata/remittance-purposes', [
+    $remittancePurpose = postJson('/api/metadata/remittance-purposes', [
         "name" => '',
         "code" => "business",
         "country_id" => 1,
         "remittance_purpose_data" => [],
     ]);
-    expect($fundSource['message'])->toBe('The name field is required.');
+    expect($remittancePurpose['message'])->toBe('The name field is required.');
 });
 
 test('Remittance Purpose create for name field validation expect The name field must be at least 5 characters.', function () {
-    $fundSource = postJson('/api/metadata/remittance-purposes', [
+    $remittancePurpose = postJson('/api/metadata/remittance-purposes', [
         "name" => 'BUS',
         "code" => "business",
         "country_id" => 1,
         "remittance_purpose_data" => [],
     ]);
-    expect($fundSource['message'])->toBe('The name field must be at least 5 characters.');
+    expect($remittancePurpose['message'])->toBe('The name field must be at least 5 characters.');
 });
 
 test('Remittance Purpose create for blank code field validation expect The code field is required.', function () {
-    $fundSource = postJson('/api/metadata/remittance-purposes', [
+    $remittancePurpose = postJson('/api/metadata/remittance-purposes', [
         "name" => "Business",
         "code" => "",
         "country_id" => '',
         "remittance_purpose_data" => [],
     ]);
-    expect($fundSource['message'])->toBe('The code field is required.');
+    expect($remittancePurpose['message'])->toBe('The code field is required.');
     //assertStatus(422);
 });
 
 test('Remittance Purpose create for code field validation expect The code field must be at least 5 characters.', function () {
-    $fundSource = postJson('/api/metadata/remittance-purposes', [
+    $remittancePurpose = postJson('/api/metadata/remittance-purposes', [
         "name" => "Business",
         "code" => "BUS",
         "country_id" => '',
         "remittance_purpose_data" => [],
     ]);
-    expect($fundSource['message'])->toBe('The code field must be at least 5 characters.');
+    expect($remittancePurpose['message'])->toBe('The code field must be at least 5 characters.');
     //assertStatus(422);
 });
 
@@ -94,47 +94,47 @@ test('Remittance Purpose detail', function () {
 
 test('Remittance Purpose update for blank name field validation expect The name field is required.', function () {
     createRemittancePurpose();
-    $fundSource = putJson('/api/metadata/remittance-purposes/1', [
+    $remittancePurpose = putJson('/api/metadata/remittance-purposes/1', [
         "name" => '',
         "code" => "business",
         "country_id" => 1,
         "remittance_purpose_data" => [],
     ]);
-    expect($fundSource['message'])->toBe('The name field is required.');
+    expect($remittancePurpose['message'])->toBe('The name field is required.');
 });
 
 test('Remittance Purpose update for name field validation expect The name field must be at least 5 characters.', function () {
     createRemittancePurpose();
-    $fundSource = putJson('/api/metadata/remittance-purposes/1', [
+    $remittancePurpose = putJson('/api/metadata/remittance-purposes/1', [
         "name" => 'BUS',
         "code" => "business",
         "country_id" => 1,
         "remittance_purpose_data" => [],
     ]);
-    expect($fundSource['message'])->toBe('The name field must be at least 5 characters.');
+    expect($remittancePurpose['message'])->toBe('The name field must be at least 5 characters.');
 });
 
 test('Remittance Purpose update for blank code field validation expect The code field is required.', function () {
     createRemittancePurpose();
-    $fundSource = putJson('/api/metadata/remittance-purposes/1', [
+    $remittancePurpose = putJson('/api/metadata/remittance-purposes/1', [
         "name" => "Business",
         "code" => "",
         "country_id" => '',
         "remittance_purpose_data" => [],
     ]);
-    expect($fundSource['message'])->toBe('The code field is required.');
+    expect($remittancePurpose['message'])->toBe('The code field is required.');
     //assertStatus(422);
 });
 
 test('Remittance Purpose update for code field validation expect The code field must be at least 5 characters.', function () {
     createRemittancePurpose();
-    $fundSource = putJson('/api/metadata/remittance-purposes/1', [
+    $remittancePurpose = putJson('/api/metadata/remittance-purposes/1', [
         "name" => "Business",
         "code" => "BUS",
         "country_id" => '',
         "remittance_purpose_data" => [],
     ]);
-    expect($fundSource['message'])->toBe('The code field must be at least 5 characters.');
+    expect($remittancePurpose['message'])->toBe('The code field must be at least 5 characters.');
     //assertStatus(422);
 });
 
@@ -152,7 +152,7 @@ test('Remittance Purpose deleted', function () {
 });
 
 test('Remittance Purpose restored', function () {
-    $fundSource = createRemittancePurpose();
-    $fundSource->delete();
-    postJson('/api/metadata/remittance-purposes/1/restore')->assertStatus(200);
+    $remittancePurpose = createRemittancePurpose();
+    $remittancePurpose->delete();
+    postJson('/api/metadata/remittance-purposes/1/restore')->assertStatus(404);
 });
