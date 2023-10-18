5e5390f8354c9e31fc4a59bb3a26dbdad3e6ece8
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-18 08:25:08 +0600

    LP-43 Call Recipient Relation Model Class for Create Test Case

--- tests/Feature/RecipientRelationTest.php
+++ tests/Feature/RecipientRelationTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 use function Pest\Laravel\deleteJson;
@@ -7,3 +8,12 @@ use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 use function Pest\Laravel\putJson;
 
+function createRelations(): Model|\MongoDB\Laravel\Eloquent\Model|null
+{
+    return \Fintech\MetaData\Facades\MetaData::relation()->create([
+        "name" => Str::random(20),
+        "code" => "SOF-SOF",
+        "country_id" => 1,
+        "relation_data" => [],
+    ]);
+}
