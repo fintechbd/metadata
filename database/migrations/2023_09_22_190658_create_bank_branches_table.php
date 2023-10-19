610c3dfc652b535314fcbd4660f436a4334aef6d
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-20 01:53:25 +0600

    LP-105 bank branch migration

--- database/migrations/2023_09_22_190658_create_bank_branches_table.php
+++ database/migrations/2023_09_22_190658_create_bank_branches_table.php
@@ -12,22 +12,11 @@ return new class () extends Migration {
     {
         Schema::create('bank_branches', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('bank_id')->nullable();
             $table->string('bank_branch_name');
-            $table->string('bank_branch_iso2')->nullable();
-            $table->string('bank_branch_iso3')->nullable();
-            $table->string('bank_branch_num_code')->nullable();
-            $table->string('bank_branch_phone_code')->nullable();
-            $table->string('bank_branch_capital')->nullable();
-            $table->string('bank_branch_currency')->nullable();
-            $table->string('bank_branch_region')->nullable();
-            $table->string('bank_branch_subregion')->nullable();
-            $table->string('bank_branch_currency_symbol')->nullable();
-            $table->string('bank_branch_language')->nullable();
-            $table->string('bank_branch_logo')->nullable();
-            $table->string('bank_branch_status')->nullable();
-            $table->json('bank_branch_timezones')->nullable();
-            $table->json('bank_branch_translations')->nullable();
-            $table->json('bank_branch_data')->nullable();
+            $table->json('bank_branch_json_data')->nullable();
+            $table->json('bank_branch_json_data_value')->nullable();
+            $table->boolean('enabled')->nullable();
             $table->unsignedBigInteger('creator_id')->index()->nullable();
             $table->unsignedBigInteger('editor_id')->index()->nullable();
             $table->unsignedBigInteger('destroyer_id')->index()->nullable();
