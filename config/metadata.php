da0993fe745ea292f9b20daf7819aad2d288f26d
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-21 02:00:20 +0600

    config lock added in all package

--- config/metadata.php
+++ config/metadata.php
@@ -3,14 +3,22 @@
 return [
 
     /*
-     * |--------------------------------------------------------------------------
-     * | MetaData Group Root Prefix
-     * |--------------------------------------------------------------------------
-     * |
-     * | This value will be added to your all routes from this package
-     * | Example: APP_URL/{root_prefix}/api/{version}/metadata/action
-     * |
-     * | Note: while adding prefix add closing ending slash '/'
+    |--------------------------------------------------------------------------
+    | Enable Module APIs
+    |--------------------------------------------------------------------------
+    | this setting enable the api will be available or not
+    */
+    'enabled' => true,
+
+    /*
+     |--------------------------------------------------------------------------
+     | MetaData Group Root Prefix
+     |--------------------------------------------------------------------------
+     |
+     | This value will be added to your all routes from this package
+     | Example: APP_URL/{root_prefix}/api/{version}/metadata/action
+     |
+     | Note: while adding prefix add closing ending slash '/'
      */
     'root_prefix' => '',
 
