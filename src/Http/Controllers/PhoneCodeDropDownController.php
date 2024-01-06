75797613231c7273598b4bdbbf6dfbb4a3235529
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-01-06 12:03:54 +0600

    App Controller namespace issue fixed

--- src/Http/Controllers/PhoneCodeDropDownController.php
+++ src/Http/Controllers/PhoneCodeDropDownController.php
@@ -2,13 +2,13 @@
 
 namespace Fintech\MetaData\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Exception;
 use Fintech\Core\Http\Requests\DropDownRequest;
 use Fintech\Core\Http\Resources\DropDownCollection;
 use Fintech\Core\Traits\ApiResponseTrait;
 use Fintech\MetaData\Facades\MetaData;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller;
 
 class PhoneCodeDropDownController extends Controller
 {
