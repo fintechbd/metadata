08d2c716b7885e95eee4d67befdeb00758e429ff
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-20 02:37:52 +0600

    unused php doc block removed

--- src/MetaData.php
+++ src/MetaData.php
@@ -18,114 +18,75 @@ use Fintech\MetaData\Services\SubregionService;
 
 class MetaData
 {
-    /**
-     * @return RegionService
-     *
-     */
+
     public function region($filters = null)
     {
         return \singleton(RegionService::class, $filters);
     }
 
-    /**
-     * @return SubregionService
-     *
-     */
+
     public function subregion($filters = null)
     {
         return \singleton(SubregionService::class, $filters);
     }
 
-    /**
-     * @return CountryService
-     *
-     */
+
     public function country($filters = null)
     {
         return \singleton(CountryService::class, $filters);
     }
 
-    /**
-     * @return StateService
-     *
-     */
+
     public function state($filters = null)
     {
         return \singleton(StateService::class, $filters);
     }
 
-    /**
-     * @return CityService
-     *
-     */
+
     public function city($filters = null)
     {
         return \singleton(CityService::class, $filters);
     }
 
-    /**
-     * @return FundSourceService
-     *
-     */
+
     public function fundSource($filters = null)
     {
         return \singleton(FundSourceService::class, $filters);
     }
 
-    /**
-     * @return OccupationService
-     *
-     */
+
     public function occupation($filters = null)
     {
         return \singleton(OccupationService::class, $filters);
     }
 
-    /**
-     * @return RelationService
-     *
-     */
+
     public function relation($filters = null)
     {
         return \singleton(RelationService::class, $filters);
     }
 
-    /**
-     * @return RemittancePurposeService
-     *
-     */
+
     public function remittancePurpose($filters = null)
     {
         return \singleton(RemittancePurposeService::class, $filters);
     }
 
-    /**
-     * @return CatalogService
-     */
     public function language($filters = null)
     {
         return \singleton(LanguageService::class, $filters);
     }
 
-    /**
-     * @return CatalogService
-     */
     public function catalog($filters = null)
     {
         return \singleton(CatalogService::class, $filters);
     }
 
-    /**
-     * @return CurrencyService
-     */
     public function currency($filters = null)
     {
         return \singleton(CurrencyService::class, $filters);
     }
 
-    /**
-     * @return IdDocTypeService
-     */
     public function idDocType($filters = null)
     {
         return \singleton(IdDocTypeService::class, $filters);
